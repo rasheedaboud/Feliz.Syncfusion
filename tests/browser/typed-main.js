3ab125bdeea5dbfb54ac3cc80c0487660f380a02
Rasheed Aboud <rasheedaboud@gmail.com>
2026-09-23 17:11:10 -0600

    Expose typed component diagnostic controls

--- tests/browser/typed-main.js
+++ tests/browser/typed-main.js
@@ -1,7 +1,10 @@
 import "@syncfusion/ej2/material.css";
-import { mountAll, updateAll, dataQueryCount } from "../../artifacts/contracts/Contracts.js";
+import { mountAll, updateAll, updateComponent, dataQueryCount, dataQueryDiagnostics } from "../../artifacts/contracts/Contracts.js";
 
 mountAll();
 window.__typedReady = true;
 window.__typedUpdateAll = updateAll;
 window.__typedDataQueryCount = dataQueryCount;
+
+window.__typedUpdateComponent = updateComponent;
+window.__typedDataQueryDiagnostics = dataQueryDiagnostics;
