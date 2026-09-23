8d6b5a951a341b6f5be1c4cc01d9978403bda7ef
Rasheed Aboud <rasheedaboud@gmail.com>
2026-09-23 16:56:49 -0600

    Add SfKanban.fs typed Feliz binding

---
+++ Felize.Syncfusion/SfKanban.fs
@@ -0,0 +1,52 @@
+namespace Syncfusion
+
+open Fable.Core
+open Fable.Core.JsInterop
+open Feliz
+
+type ISfKanbanProperty = interface end
+
+module SfKanban =
+    let private component: obj = import "KanbanComponent" "@syncfusion/ej2-react-kanban"
+    let private columnsDirective: obj = import "ColumnsDirective" "@syncfusion/ej2-react-kanban"
+    let private columnDirective: obj = import "ColumnDirective" "@syncfusion/ej2-react-kanban"
+
+    [<AllowNullLiteral>]
+    type CardClickEventArgs<'T> =
+        abstract data: 'T
+        abstract cancel: bool with get, set
+
+    [<AllowNullLiteral>]
+    type ActionEventArgs<'T> =
+        abstract requestType: string
+        abstract changedRecords: ResizeArray<'T> option
+
+    type CardSettings = {
+        headerField: string
+        contentField: string
+    }
+
+    type Column = {
+        headerText: string
+        keyField: string
+    }
+
+    [<Erase>]
+    type prop =
+        static member inline keyField(value: string) : ISfKanbanProperty = unbox ("keyField", value)
+        static member inline dataSource(value: 'T array) : ISfKanbanProperty = unbox ("dataSource", value)
+        static member inline cardSettings(value: CardSettings) : ISfKanbanProperty = unbox ("cardSettings", value)
+        static member inline allowDragAndDrop(value: bool) : ISfKanbanProperty = unbox ("allowDragAndDrop", value)
+        static member inline cssClass(value: string) : ISfKanbanProperty = unbox ("cssClass", value)
+        static member inline cardClick(callback: CardClickEventArgs<'T> -> unit) : ISfKanbanProperty = unbox ("cardClick", callback)
+        static member inline actionComplete(callback: ActionEventArgs<'T> -> unit) : ISfKanbanProperty = unbox ("actionComplete", callback)
+        static member inline children(value: ReactElement list) : ISfKanbanProperty = unbox ("children", value)
+
+    let inline column (column: Column) : ReactElement =
+        ReactLegacy.createElement(unbox columnDirective, createObj [ "headerText" ==> column.headerText; "keyField" ==> column.keyField ])
+
+    let inline columns (items: ReactElement list) : ReactElement =
+        ReactLegacy.createElement(unbox columnsDirective, createObj [ "children" ==> items ])
+
+    let inline create (props: ISfKanbanProperty list) : ReactElement =
+        ReactLegacy.createElement(unbox component, createObj !!props)
