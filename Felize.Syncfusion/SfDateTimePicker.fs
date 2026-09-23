c529566b6adc6576b3212382a415ecd1abd9968c
Rasheed Aboud <rasheedaboud@gmail.com>
2026-09-23 16:56:46 -0600

    Add SfDateTimePicker.fs typed Feliz binding

---
+++ Felize.Syncfusion/SfDateTimePicker.fs
@@ -0,0 +1,32 @@
+namespace Syncfusion
+
+open System
+open Fable.Core
+open Fable.Core.JsInterop
+open Feliz
+open Browser.Types
+
+type ISfDateTimePickerProperty = interface end
+
+module SfDateTimePicker =
+    let private component: obj = import "DateTimePickerComponent" "@syncfusion/ej2-react-calendars"
+
+    [<AllowNullLiteral>]
+    type ChangeEventArgs =
+        abstract value: DateTime option
+        abstract element: HTMLInputElement option
+        abstract isInteracted: bool option
+
+    [<Erase>]
+    type prop =
+        static member inline value(value: DateTime) : ISfDateTimePickerProperty = unbox ("value", value)
+        static member inline format(value: string) : ISfDateTimePickerProperty = unbox ("format", value)
+        static member inline placeholder(value: string) : ISfDateTimePickerProperty = unbox ("placeholder", value)
+        static member inline readonly(value: bool) : ISfDateTimePickerProperty = unbox ("readonly", value)
+        static member inline enabled(value: bool) : ISfDateTimePickerProperty = unbox ("enabled", value)
+        static member inline step(value: int) : ISfDateTimePickerProperty = unbox ("step", value)
+        static member inline cssClass(value: string) : ISfDateTimePickerProperty = unbox ("cssClass", value)
+        static member inline change(callback: ChangeEventArgs -> unit) : ISfDateTimePickerProperty = unbox ("change", callback)
+
+    let inline create (props: ISfDateTimePickerProperty list) : ReactElement =
+        ReactLegacy.createElement(unbox component, createObj !!props)
