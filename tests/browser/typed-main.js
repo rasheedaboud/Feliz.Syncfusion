2f7a0e0220ace2d31ec8d425452f9a8e1bf1b76c
Rasheed Aboud <rasheedaboud@gmail.com>
2026-09-23 17:02:45 -0600

    Load typed Fable contracts in browser

---
+++ tests/browser/typed-main.js
@@ -0,0 +1,7 @@
+import "@syncfusion/ej2/material.css";
+import { mountAll, updateAll, dataQueryCount } from "../../artifacts/contracts/Contracts.js";
+
+mountAll();
+window.__typedReady = true;
+window.__typedUpdateAll = updateAll;
+window.__typedDataQueryCount = dataQueryCount;
