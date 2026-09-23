378f4170a4840c9ae27c72ff0b6a64ee7b52ac2e
Rasheed Aboud <rasheedaboud@gmail.com>
2026-09-23 16:56:44 -0600

    Add SfMultiSelect.fs typed Feliz binding

---
+++ Felize.Syncfusion/SfMultiSelect.fs
@@ -0,0 +1,33 @@
+namespace Syncfusion
+
+open Fable.Core
+open Fable.Core.JsInterop
+open Feliz
+
+type ISfMultiSelectProperty = interface end
+
+module SfMultiSelect =
+    let private component: obj = import "MultiSelectComponent" "@syncfusion/ej2-react-dropdowns"
+
+    [<StringEnum; RequireQualifiedAccess>]
+    type Mode = Box | Default | Delimiter | CheckBox
+
+    [<AllowNullLiteral>]
+    type ChangeEventArgs =
+        abstract value: obj option
+        abstract isInteracted: bool option
+
+    [<Erase>]
+    type prop =
+        static member inline dataSource(value: 'T array) : ISfMultiSelectProperty = unbox ("dataSource", value)
+        static member inline value(value: string array) : ISfMultiSelectProperty = unbox ("value", value)
+        static member inline placeholder(value: string) : ISfMultiSelectProperty = unbox ("placeholder", value)
+        static member inline mode(value: Mode) : ISfMultiSelectProperty = unbox ("mode", value)
+        static member inline readonly(value: bool) : ISfMultiSelectProperty = unbox ("readonly", value)
+        static member inline enabled(value: bool) : ISfMultiSelectProperty = unbox ("enabled", value)
+        static member inline allowCustomValue(value: bool) : ISfMultiSelectProperty = unbox ("allowCustomValue", value)
+        static member inline cssClass(value: string) : ISfMultiSelectProperty = unbox ("cssClass", value)
+        static member inline change(callback: ChangeEventArgs -> unit) : ISfMultiSelectProperty = unbox ("change", callback)
+
+    let inline create (props: ISfMultiSelectProperty list) : ReactElement =
+        ReactLegacy.createElement(unbox component, createObj !!props)
