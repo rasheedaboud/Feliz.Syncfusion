27d99e8d9ba6e4ce34da257e0b0c7fafca39e9da
Rasheed Aboud <rasheedaboud@gmail.com>
2026-09-23 18:40:53 -0600

    Use deterministic click-open Tooltip contract

--- tests/contracts/Contracts.fs
+++ tests/contracts/Contracts.fs
@@ -73,7 +73,7 @@ let private renderTooltip (content: string) =
     Tooltip.create [
         Tooltip.prop.content content
         Tooltip.prop.position Tooltip.Position.TopCenter
-        Tooltip.prop.opensOn Tooltip.OpensOn.Hover
+        Tooltip.prop.opensOn Tooltip.OpensOn.Click
         Tooltip.prop.showTipPointer true
         Tooltip.prop.target "#typed-tooltip-target"
         Tooltip.prop.children [ Html.button [ prop.id "typed-tooltip-target"; prop.text "Hover target" ] ]
