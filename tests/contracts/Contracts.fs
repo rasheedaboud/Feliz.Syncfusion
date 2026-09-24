3bbb6489663df3641349f8220e6341596705b2d8
Rasheed Aboud <rasheedaboud@gmail.com>
2026-09-23 18:32:51 -0600

    Use explicit Tooltip target in typed contract

--- tests/contracts/Contracts.fs
+++ tests/contracts/Contracts.fs
@@ -75,8 +75,8 @@ let private renderTooltip (content: string) =
         Tooltip.prop.position Tooltip.Position.TopCenter
         Tooltip.prop.opensOn Tooltip.OpensOn.Hover
         Tooltip.prop.showTipPointer true
-        Tooltip.prop.target "button"
-        Tooltip.prop.children [ Html.button [ prop.text "Hover target" ] ]
+        Tooltip.prop.target "#typed-tooltip-target"
+        Tooltip.prop.children [ Html.button [ prop.id "typed-tooltip-target"; prop.text "Hover target" ] ]
     ]
 
 let private renderKanban rows =
