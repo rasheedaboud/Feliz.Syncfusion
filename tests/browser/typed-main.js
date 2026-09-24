ce061abd850e295125c7ae7e179b784cb9093199
Rasheed Aboud <rasheedaboud@gmail.com>
2026-09-23 18:32:43 -0600

    Mount one typed Syncfusion contract per browser test

--- tests/browser/typed-main.js
+++ tests/browser/typed-main.js
@@ -1,10 +1,22 @@
 import "@syncfusion/ej2/material.css";
-import { mountAll, updateAll, updateComponent, dataQueryCount, dataQueryDiagnostics } from "../../artifacts/contracts/Contracts.js";
+import {
+  mountAll,
+  mountComponent,
+  updateAll,
+  updateComponent,
+  dataQueryCount,
+  dataQueryDiagnostics
+} from "../../artifacts/contracts/Contracts.js";
 
-mountAll();
+const component = new URLSearchParams(window.location.search).get("component");
+if (component) {
+  mountComponent(component);
+} else {
+  mountAll();
+}
+
 window.__typedReady = true;
 window.__typedUpdateAll = updateAll;
-window.__typedDataQueryCount = dataQueryCount;
-
 window.__typedUpdateComponent = updateComponent;
+window.__typedDataQueryCount = dataQueryCount;
 window.__typedDataQueryDiagnostics = dataQueryDiagnostics;
